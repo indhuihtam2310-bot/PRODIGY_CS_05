eefbf2e12f195ceb4771ed77feb3e60d98c20cd3
IndhuMathi <indhuihtam2310@gmail.com>
2025-11-11 13:24:23 +0530

    Improve comment formatting in Network_Packet_Analyzer.py
    Reformatted comments for clarity and readability.

--- Network_Packet_Analyzer.py
+++ Network_Packet_Analyzer.py
@@ -1,5 +1,6 @@
-# PRODIGY_CS_05
-# I Have Developed a packet Sniffer tool that sapture and analyzes network packet, It display the infomation of source and destination IP addresses, Protocols and payload data.
+# PRODIGY_CS_05 
+# I Have Developed a packet Sniffer tool that sapture and analyzes network packet,
+#It display the infomation of source and destination IP addresses, Protocols and payload data.
 
 import os
 import sys
